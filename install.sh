f909740ca5a12ca7cb29f0da847905e311752b71
Bryce <brycekbargar@gmail.com>
2015-05-16 23:16:18 -0500

    Added the startup script. Did some work with the naming. Moved the package manager update to the ensure-package-manager script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Run to setup dotfiles
+# Right now it just acts as a chell container so I can test exits
+#
+# Dependencies
+source ensure-lastpass-cli
+# source src/ensure-dropbox
+
+# Do some fancy stuff with ssh keys and symlinking dotfiles
