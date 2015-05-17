ff16cc23087d289574a9498afadb06be1237beed
Bryce <brycekbargar@gmail.com>
2015-05-17 11:05:17 -0500

    Add the github api token to homebrew

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 # Right now it just acts as a chell container so I can test exits
 #
 # Dependencies
+source ensure-homebrew-github-api-token
 source ensure-dropbox
 
 # Do some fancy stuff with ssh keys
