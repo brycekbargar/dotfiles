9c756051617e6d28fc76a708a79a7ad5352d75ce
Bryce <brycekbargar@gmail.com>
2015-05-23 16:22:20 -0500

    Standardized the header format

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 # Run to setup dotfiles
 # Right now it just acts as a shell container so I can test exits
 #
-# Dependencies
 source init
+
 requires homebrew-github-api-token
 
 # Do some fancy stuff with ssh keys
