13fcc5dcc1e285d0652a80283b967d90c54bd26f
Bryce <brycekbargar@gmail.com>
2016-04-26 23:43:08 -0500

    Fix the Pager

--- dotfiles/zshrcfiles/exports.zsh
+++ dotfiles/zshrcfiles/exports.zsh
@@ -5,6 +5,6 @@ export LSCOLORS=Gxfxcxdxbxegedabagacad
 export GREP_OPTIONS='--color=auto'
 export GREP_COLOR='3;33'
 
-export LESS='--ignore-case --raw-control-chars'
+export LESS='--ignore-case --raw-control-chars -FRXK'
 export PAGER='less'
 export EDITOR='nvim'
