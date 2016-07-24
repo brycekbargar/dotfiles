83e78bb7c5bb91d4fbb329fd911414f444d66240
bryce <brycekbargar@gmail.com>
2016-07-24 17:00:25 -0500

    Setup git

--- src/install.sh
+++ src/install.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 
 # Set the path to the src folder depending on where the script is run fromj
-srcDir="$(dirname $0)"
+SRC="$(dirname $0)"
+DOTFILES="$(dirname $0)/../dotfiles/"
 
-source "$srcDir/utils/logging"
+source "$SRC/utils/logging"
 
 log_step "01 Set Environment"
-source "$srcDir/utils/set-environment"
+source "$SRC/utils/set-environment"
 
 log_step "02 Setup Package Manager"
-source "$srcDir/$ENVIRONMENT/02-setup-package-manager"
+source "$SRC/$ENVIRONMENT/02-setup-package-manager"
 
 log_step "03 Sync SSH Keys"
-source "$srcDir/$ENVIRONMENT/03-sync-ssh-keys"
+source "$SRC/$ENVIRONMENT/03-sync-ssh-keys"
 
 log_step "04 Install git"
-source "$srcDir/$ENVIRONMENT/04-install-git"
+source "$SRC/$ENVIRONMENT/04-install-git"
 
 log_step "05 Install zsh"
-source "$srcDir/$ENVIRONMENT/05-install-zsh"
+source "$SRC/$ENVIRONMENT/05-install-zsh"
 
 log_step "06 Install tmux"
-source "$srcDir/$ENVIRONMENT/06-install-tmux"
+source "$SRC/$ENVIRONMENT/06-install-tmux"
 
 log_step "07 Install neovim"
-source "$srcDir/$ENVIRONMENT/07-install-neovim"
+source "$SRC/$ENVIRONMENT/07-install-neovim"
 
 log_step "08 Install Environment Specific Packages"
-source "$srcDir/$ENVIRONMENT/08-packages"
+source "$SRC/$ENVIRONMENT/08-packages"
 
 exit 0
