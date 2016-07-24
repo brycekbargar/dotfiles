f534493c1013b9026fa124e3797625458b29dac6
bryce <brycekbargar@gmail.com>
2016-07-24 16:16:09 -0500

    Seriously fix the paths this time

--- src/install.sh
+++ src/install.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-local repoDir="$(pwd)"
-local srcDir="$repoDir/src"
+# Set the path to the src folder depending on where the script is run fromj
+srcDir="$(dirname $0)"
 
-source "$repoDir/utils/logging"
+source "$srcDir/utils/logging"
 
 log_step "01 Set Environment"
-source "$repoDir/utils/set-environment"
+source "$srcDir/utils/set-environment"
 
 log_step "02 Setup Package Manager"
-source "$repoDir/$ENVIRONMENT/02_setup-package-manager"
+source "$srcDir/$ENVIRONMENT/02_setup-package-manager"
 
 log_step "03 Sync SSH Keys"
-source "$repoDir/$ENVIRONMENT/03_sync-ssh-keys"
+source "$srcDir/$ENVIRONMENT/03_sync-ssh-keys"
 
 log_step "04 Install git"
-source "$repoDir/$ENVIRONMENT/04_install-git"
+source "$srcDir/$ENVIRONMENT/04_install-git"
 
 log_step "05 Install zsh"
-source "$repoDir/$ENVIRONMENT/05_install-zsh"
+source "$srcDir/$ENVIRONMENT/05_install-zsh"
 
 log_step "06 Install tmux"
-source "$repoDir/$ENVIRONMENT/06_install-tmux"
+source "$srcDir/$ENVIRONMENT/06_install-tmux"
 
 log_step "07 Install neovim"
-source "$repoDir/$ENVIRONMENT/07_install-neovim"
+source "$srcDir/$ENVIRONMENT/07_install-neovim"
 
 log_step "08 Install Environment Specific Packages"
-source "$repoDir/$ENVIRONMENT/08_packages"
+source "$srcDir/$ENVIRONMENT/08_packages"
 
 exit 0
