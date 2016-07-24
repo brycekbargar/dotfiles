90518723afbc2b5433d5f2a2aa9e7b8af8675b99
bryce <brycekbargar@gmail.com>
2016-07-24 16:19:38 -0500

    Use consistent naming

--- src/install.sh
+++ src/install.sh
@@ -9,24 +9,24 @@ log_step "01 Set Environment"
 source "$srcDir/utils/set-environment"
 
 log_step "02 Setup Package Manager"
-source "$srcDir/$ENVIRONMENT/02_setup-package-manager"
+source "$srcDir/$ENVIRONMENT/02-setup-package-manager"
 
 log_step "03 Sync SSH Keys"
-source "$srcDir/$ENVIRONMENT/03_sync-ssh-keys"
+source "$srcDir/$ENVIRONMENT/03-sync-ssh-keys"
 
 log_step "04 Install git"
-source "$srcDir/$ENVIRONMENT/04_install-git"
+source "$srcDir/$ENVIRONMENT/04-install-git"
 
 log_step "05 Install zsh"
-source "$srcDir/$ENVIRONMENT/05_install-zsh"
+source "$srcDir/$ENVIRONMENT/05-install-zsh"
 
 log_step "06 Install tmux"
-source "$srcDir/$ENVIRONMENT/06_install-tmux"
+source "$srcDir/$ENVIRONMENT/06-install-tmux"
 
 log_step "07 Install neovim"
-source "$srcDir/$ENVIRONMENT/07_install-neovim"
+source "$srcDir/$ENVIRONMENT/07-install-neovim"
 
 log_step "08 Install Environment Specific Packages"
-source "$srcDir/$ENVIRONMENT/08_packages"
+source "$srcDir/$ENVIRONMENT/08-packages"
 
 exit 0
