372fd902b318f3acf19d3cd2812cc70ef5e49a20
bryce <brycekbargar@gmail.com>
2016-07-24 22:33:59 -0500

    Change terminal colors

--- dotfiles/zsh/shared/prompt.zsh
+++ dotfiles/zsh/shared/prompt.zsh
@@ -21,8 +21,8 @@ function precmd {
   local -ah ps1
   ps1=(
     '%B'
-    '%F{047}%n@%m'
-    ' %(?.%F{099}.%F{196})' "$last_cmd_text"
+    '%F{047}%n%F{199}@%F{047}%m'
+    ' %(?.%F{099}.%F{160})' "$last_cmd_text"
     ' %F{075}$(pwd | sed -e "s,^$HOME,~,")'
     ' %F{192}${vcs_info_msg_0_/ U/ *}'
     "%f%b"
