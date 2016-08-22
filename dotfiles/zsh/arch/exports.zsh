0ea98158c4c134271f11f7025ac1b86c2ff73461
bryce <brycekbargar@gmail.com>
2016-08-22 00:07:31 +0000

    Setup ls colors and alias

---
+++ dotfiles/zsh/arch/exports.zsh
@@ -0,0 +1,3 @@
+export LS_COLORS='di=1:fi=0:ln=31:pi=5:so=5:bd=5:cd=5:or=31:mi=0:ex=35'
+
+alias ls='ls -lA --color'
---
+++ dotfiles/zsh/bsd/exports.zsh
@@ -0,0 +1,5 @@
+export CLICOLOR=1
+export LSCOLORS=Gxfxcxdxbxegedabagacad
+
+alias ls='ls -lA'
+
