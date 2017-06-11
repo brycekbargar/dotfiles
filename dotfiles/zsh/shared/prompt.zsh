e45c0a4a7e4049714524c9774a554e55b46a8e0a
bryce <brycekbargar@gmail.com>
2017-06-10 19:26:24 -0500

    Disable git dirty checking

--- dotfiles/zsh/shared/prompt.zsh
+++ dotfiles/zsh/shared/prompt.zsh
@@ -3,7 +3,7 @@ autoload -Uz vcs_info
 setopt prompt_subst
 
 zstyle ':vcs_info:*' enable git
-zstyle ':vcs_info:git:*' check-for-changes true
+zstyle ':vcs_info:git:*' check-for-changes false
 zstyle ':vcs_info:git:*' formats '[%b] %u'
 zstyle ':vcs_info:git:*' actionformats '[%b] (%a)'
 
