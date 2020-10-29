6746fe3bf30525e0adb6b6ef50a88631b0680702
bryce <brycekbargar@gmail.com>
2020-10-28 20:49:19 -0500

    Fix exa aliases and minimal prompt

--- dotfiles/zsh/zshrcfiles/antibody_configuration.zsh
+++ dotfiles/zsh/zshrcfiles/antibody_configuration.zsh
@@ -1,9 +1,7 @@
-# DarrinTisdale/zsh-aliases-exa
-alias lsr='ls -lhA --color'
-
 # subnixr/minimal
-export MNML_PROMPT=(mnml_status mnml_uhp mnml_keymap)
-export MNML_RPROMPT=('mnml_cwd 4 0' mnml_git)
+export MNML_INSERT_CHAR='◻◻◻◼◻'
+export MNML_NORMAL_CHAR='◼◻◼◼◼'
+export MNML_PROMPT=("printf '|||['" mnml_status mnml_git 'mnml_cwd 6 0' "printf $']|||\n🦖'")
+export MNML_RPROMPT=(mnml_keymap)
 export MNML_INFOLN=()
 export MNML_MAGICENTER=()
-
