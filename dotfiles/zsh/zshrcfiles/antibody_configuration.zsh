3e545bcb5060ed9dd4aa17f919d12c26f99a0ed7
bryce <brycekbargar@gmail.com>
2020-10-28 19:29:54 -0500

    Change antibody plugins

--- dotfiles/zsh/zshrcfiles/antibody_configuration.zsh
+++ dotfiles/zsh/zshrcfiles/antibody_configuration.zsh
@@ -1,10 +1,6 @@
 # subnixr/minimal
-export MNML_PROMPT=(mnml_status mnml_keymap)
-export MNML_RPROMPT=('mnml_cwd 4 0' mnml_me_git)
-export MNML_INFOLN=(mnml_err mnml_jobs mnml_uhp)
+export MNML_PROMPT=(mnml_status mnml_uhp mnml_keymap)
+export MNML_RPROMPT=('mnml_cwd 4 0' mnml_git)
+export MNML_INFOLN=()
 export MNML_MAGICENTER=()
-
-# zsh-users/zsh-syntax-highlighting
-export ZSH_HIGHLIGHT_MAXLENGTH=128
-export ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
 
