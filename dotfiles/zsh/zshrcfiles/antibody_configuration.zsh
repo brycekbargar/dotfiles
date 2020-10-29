53f8d2221e276ec4dbe53b648921b461ac743a18
bryce <brycekbargar@gmail.com>
2020-10-28 19:46:00 -0500

    Install stable debian packages (and some testing)

--- dotfiles/zsh/zshrcfiles/antibody_configuration.zsh
+++ dotfiles/zsh/zshrcfiles/antibody_configuration.zsh
@@ -1,3 +1,6 @@
+# DarrinTisdale/zsh-aliases-exa
+alias lsr='ls -lhA --color'
+
 # subnixr/minimal
 export MNML_PROMPT=(mnml_status mnml_uhp mnml_keymap)
 export MNML_RPROMPT=('mnml_cwd 4 0' mnml_git)
