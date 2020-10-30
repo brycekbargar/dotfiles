3899b36bd5785991b0e7d46342ada4a1497c4779
bryce <brycekbargar@gmail.com>
2020-10-30 16:07:17 -0700

    Prompt changes

--- dotfiles/zsh/zshrcfiles/antibody_configuration.zsh
+++ dotfiles/zsh/zshrcfiles/antibody_configuration.zsh
@@ -1,7 +1,7 @@
 # subnixr/minimal
-export MNML_INSERT_CHAR='◻◻◻◼◻'
-export MNML_NORMAL_CHAR='◼◻◼◼◼'
-export MNML_PROMPT=("printf '|||['" mnml_status mnml_git 'mnml_cwd 6 0' "printf $']|||\n🦖'")
+export MNML_INSERT_CHAR='⬛⬜⬛⬛⬛'
+export MNML_NORMAL_CHAR='⬛⬛⬛⬛⬛'
+export MNML_PROMPT=("printf '['" mnml_status mnml_git 'mnml_cwd 6 0' "printf $']\n🦖'")
 export MNML_RPROMPT=(mnml_keymap)
 export MNML_INFOLN=()
 export MNML_MAGICENTER=()
