d99ba90eab9b7502c75f2ea91b81fc9f199af572
bryce <brycekbargar@gmail.com>
2020-11-02 18:40:14 -0600

    Switch prompt lambda

--- dotfiles/zsh/zshrcfiles/antibody_configuration.zsh
+++ dotfiles/zsh/zshrcfiles/antibody_configuration.zsh
@@ -1,6 +1,7 @@
 # subnixr/minimal
 export MNML_INSERT_CHAR='⬛⬜⬛⬛⬛'
 export MNML_NORMAL_CHAR='⬛⬛⬛⬛⬛'
+export MNML_USER_CHAR='⚧'
 export MNML_PROMPT=("printf '['" mnml_status mnml_git 'mnml_cwd 6 0' "printf $']\n🦖'")
 export MNML_RPROMPT=(mnml_keymap)
 export MNML_INFOLN=()
