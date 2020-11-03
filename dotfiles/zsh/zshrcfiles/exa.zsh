20c2c2e97681fe43644ef016a94d33463595d762
bryce <brycekbargar@gmail.com>
2020-11-02 18:52:52 -0600

    More exa yak shaving

--- dotfiles/zsh/zshrcfiles/exa.zsh
+++ dotfiles/zsh/zshrcfiles/exa.zsh
@@ -1,6 +1,6 @@
 alias ls-raw='/bin/ls -lhA --color'
 
 alias exa-ls='exa --binary --all --extended --classify --git --group-directories-first'
-alias ls='exa-ls --long --tree --level=2'
-alias lsg='exa-ls --long --grid'
+alias ls='exa-ls --long --grid'
 alias lst='exa-ls --tree --level=4'
+alias lslt='exa-ls --long --tree --level=2'
