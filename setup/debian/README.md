defbdb179b92ecb8b7c1017810c5ffa9a2c7693b
bryce <brycekbargar@gmail.com>
2022-06-19 07:59:22 -0400

    Fix the debian setup script

--- setup/debian/README.md
+++ setup/debian/README.md
@@ -7,5 +7,12 @@ sudo apt update
 sudo apt --yes install curl
 
 curl --proto '=https' --tlsv1.2 -sSf  https://raw.githubusercontent.com/brycekbargar/dotfiles/main/setup/debian/setup.sh | sh
+
+exec zsh
+cd ~/_src/dotfiles
+conda activate dotfiles
+
+sudo apt install python3-apt
+ansible-playbook playbooks/home.playbook.yml
 ```
 
--- setup/debian/setup.sh
+++ setup/debian/setup.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 sudo apt --yes install git zsh
-git clone https://github.com/brycekbargar/dotfiles.git ~/_src/dotfiles
-ln -s ~/_src/dotfiles/HOME/dot_zshrc ~/.zshrc
-curl --proto '=https' --tlsv1.2 -sSf  https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh | sh
-/usr/bin/zsh -c "~/miniconda3/bin/conda init zsh"
 
+git clone https://github.com/brycekbargar/dotfiles.git ~/_src/dotfiles
+ln -s ~/_src/dotfiles/HOME/dot_zshenv ~/.zshenv
+mkdir -p ~/.config/zsh
+echo "#!/usr/bin/zsh" > ~/.config/zsh/.zshrc
+
+pushd /etc
+	curl -O --proto '=https' --tlsv1.2 -sSf  https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+	bash Miniconda3-latest-Linux-x86_64.sh
+popd
+/usr/bin/zsh -c "~/miniconda3/bin/conda init zsh"
+/usr/bin/zsh -c "conda env create --file ~/_src/dotfiles/environment.yml"
+/usr/bin/zsh -c "conda run -n dotfiles sudo apt install python3-apt"
