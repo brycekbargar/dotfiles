ff1a958de1bb2115a1581eb06c593c859ce7456e
bryce <brycekbargar@gmail.com>
2022-06-19 08:31:26 -0400

    Actually fix setup

--- setup/debian/README.md
+++ setup/debian/README.md
@@ -4,12 +4,14 @@
 
 ```bash
 sudo apt update; sudo apt install curl --yes
+#export DOTFILES_BRANCH=ansible-rewrite && curl --proto '=https' --tlsv1.2 -sSf  https://raw.githubusercontent.com/brycekbargar/dotfiles/"$DOTFILES_BRANCH"/setup/debian/setup.sh | bash
 curl --proto '=https' --tlsv1.2 -sSf  https://raw.githubusercontent.com/brycekbargar/dotfiles/main/setup/debian/setup.sh | bash
 
 exec zsh
 cd ~/_src/dotfiles
+conda env create --file environment.yml --quiet
 conda activate dotfiles
 
-ansible-playbook playbooks/home.playbook.yml
+ansible-playbook playbooks/home.playbook.yml --ask-become-pass
 ```
 
--- setup/debian/setup.sh
+++ setup/debian/setup.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
-sudo apt --yes install git zsh
+sudo apt --yes install git zsh python3-apt
 
-git clone https://github.com/brycekbargar/dotfiles.git ~/_src/dotfiles
+git clone -b "${DOTFILES_BRANCH:-main}" https://github.com/brycekbargar/dotfiles.git ~/_src/dotfiles
 ln -s ~/_src/dotfiles/HOME/dot_zshenv ~/.zshenv
 mkdir -p ~/.config/zsh
-echo "#!/usr/bin/zsh" > ~/.config/zsh/.zshrc
+echo '#!/usr/bin/zsh' > ~/.config/zsh/.zshrc
 
-pushd /etc
+pushd /tmp
 	curl -O --proto '=https' --tlsv1.2 -sSf  https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-	bash Miniconda3-latest-Linux-x86_64.sh -b -p ~/miniconda3
-popd /etc
+	bash Miniconda3-latest-Linux-x86_64.sh -b
+popd
 
 /usr/bin/zsh -c "~/miniconda3/bin/conda init zsh"
-/usr/bin/zsh -c "conda env create --file ~/_src/dotfiles/environment.yml"
-/usr/bin/zsh -c "conda run -n dotfiles sudo apt install python3-apt"
