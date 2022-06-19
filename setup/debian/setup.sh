a0a40c771e5834c95d4d230e7654d32d4d5ac3fe
bryce <brycekbargar@gmail.com>
2022-06-19 12:26:43 -0400

    Fix cargo bin path, file perms, and unarchive requirements

--- setup/debian/setup.sh
+++ setup/debian/setup.sh
@@ -5,6 +5,9 @@ git clone -b "${DOTFILES_BRANCH:-main}" https://github.com/brycekbargar/dotfiles
 ln -s ~/_src/dotfiles/HOME/dot_zshenv ~/.zshenv
 mkdir -p ~/.config/zsh
 echo '#!/usr/bin/zsh' > ~/.config/zsh/.zshrc
+mkdir -p ~/.local/bin
+mkdir -p ~/.local/share
+mkdir -p ~/.local/state
 
 pushd /tmp
 	curl -O --proto '=https' --tlsv1.2 -sSf  https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
