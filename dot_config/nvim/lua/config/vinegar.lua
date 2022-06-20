118d9d80aeb599d5d57044a188d859d6ec68e4f2
bryce <brycekbargar@gmail.com>
2022-06-20 16:48:36 -0400

    Simplify zshenv file and installation

--- dot_config/nvim/lua/config/vinegar.lua
+++ dot_config/nvim/lua/config/vinegar.lua
@@ -1,3 +1,4 @@
 vim.g.netrw_preview = 0
 vim.g.netrw_alto = 1
+vim.g.netrw_altfile = 1
 return function() end
--- setup/debian/setup.sh
+++ setup/debian/setup.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
-sudo apt --yes install git zsh
+mkdir ~/.config
+mkdir ~/.cache
+mkdir ~/.local
+mkdir ~/.local/share
+mkdir ~/.local/state
+mkdir ~/.local/bin
 
+sudo apt --yes install git zsh
 git clone -b "${DOTFILES_BRANCH:-main}" https://github.com/brycekbargar/dotfiles.git ~/_src/dotfiles
 ln -s ~/_src/dotfiles/HOME/dot_zshenv ~/.zshenv
 mkdir -p ~/.config/zsh
 echo '#!/usr/bin/zsh' > ~/.config/zsh/.zshrc
-mkdir -p ~/.local/bin
-mkdir -p ~/.local/share
-mkdir -p ~/.local/state
 
 pushd /tmp
 	curl -O --proto '=https' --tlsv1.2 -sSf  https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
