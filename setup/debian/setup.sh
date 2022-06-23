88bb2d4b25b0ec9c016830d123c1a02392e67c10
bryce <brycekbargar@gmail.com>
2022-06-23 19:36:23 -0400

    Better install starship

--- setup/debian/setup.sh
+++ setup/debian/setup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-mkdir -p ~/.local/var
-mkdir -p ~/.local/etc
-mkdir ~/.local/share
-mkdir ~/.local/etc/zsh
+mkdir ~/.local
+mkdir ~/.local/etc
 mkdir ~/.local/etc/conda
+mkdir ~/.local/share
+mkdir ~/.local/var
 mkdir ~/.local/var/cache
 mkdir ~/.local/var/lib
 mkdir ~/.local/bin
@@ -11,9 +11,6 @@ mkdir ~/.local/bin
 sudo apt --yes install git zsh
 git clone -b "${DOTFILES_BRANCH:-main}" https://github.com/brycekbargar/dotfiles.git ~/_src/dotfiles
 ln -s ~/_src/dotfiles/HOME/dot_zshenv ~/.zshenv
-cat > ~/.local/etc/zsh/.zshrc << EOF
-#!/usr/bin/zsh
-EOF
 cp ~/_src/dotfiles/XDG_CONFIG_HOME/conda/.condarc ~/.local/etc/conda/.condarc
 
 pushd /tmp
@@ -22,6 +19,8 @@ popd
 
 /usr/bin/zsh /tmp/Miniconda3-latest-Linux-x86_64.sh -b -s -p "~/.local/share/conda"
 /usr/bin/zsh -s << EOF
+	mkdir ~/.local/etc/zsh
+	touch ~/.local/etc/zsh/.zshrc
 	~/.local/share/conda/bin/conda init zsh
 	source ~/.local/etc/zsh/.zshrc
 	conda env create --file ~/_src/dotfiles/environment.yml --quiet
