bc2aeb7f9b960318f42f85615ab2a9a9c9f17910
bryce <brycekbargar@gmail.com>
2022-07-07 20:49:46 -0400

    Simplify setup

--- debian/README.md
+++ debian/README.md
@@ -6,11 +6,5 @@
 sudo apt update
 sudo apt install curl --yes
 export DOTFILES_BRANCH=ansible-rewrite && curl --proto '=https' --tlsv1.2 -sSf  https://raw.githubusercontent.com/brycekbargar/dotfiles/"$DOTFILES_BRANCH"/debian/setup.sh | bash
-exec zsh
-
-cd ~/_src/dotfiles
-conda activate dotfiles
-rm -fdr "$XDG_CONFIG_HOME"
-ansible-playbook playbooks/home.playbook.yml --ask-become-pass
 ```
 
--- debian/setup.sh
+++ debian/setup.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 mkdir ~/.local
-mkdir ~/.local/etc
 mkdir ~/.local/share
 mkdir ~/.local/var
 mkdir ~/.local/var/cache
@@ -9,18 +8,16 @@ mkdir ~/.local/var/lib
 sudo apt --yes install git zsh
 git clone -b "${DOTFILES_BRANCH:-main}" https://github.com/brycekbargar/dotfiles.git ~/_src/dotfiles
 ln -s ~/_src/dotfiles/.zshenv ~/.zshenv
-cp ~/_src/dotfiles/XDG_CONFIG_HOME/template.condarc ~/.local/etc/.condarc
 
 pushd /tmp
 	curl -O --proto '=https' --tlsv1.2 -sSf  https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 popd
 
-mkdir ~/.local/etc/zsh
-touch ~/.local/etc/zsh/.zshrc
-
 /usr/bin/zsh /tmp/Miniconda3-latest-Linux-x86_64.sh -b -s -p "~/.local/share/conda/base"
 /usr/bin/zsh -s << EOF
-	~/.local/share/conda/base/bin/conda init zsh
-	source ~/.local/etc/zsh/.zshrc
-	conda env create --file ~/_src/dotfiles/environment.yml --quiet
+	source <(~/.local/share/conda/base/bin/conda shell.zsh hook)
+	cd ~/_src/dotfiles
+	conda env create --file environment.yml --quiet
+	conda activate dotfiles
+	ansible-playbook playbooks/home.playbook.yml --ask-become-pass
 EOF
