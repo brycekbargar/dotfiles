3d92897e23c0dd6d8fe0b9165e234e56397d7e54
bryce <brycekbargar@gmail.com>
2022-08-17 09:50:55 -0400

    Create a bootstrap script for macos

---
+++ macos/README.md
@@ -0,0 +1,9 @@
+# Macos setup
+
+### Usage
+
+```bash
+xcode-select --install
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/brycekbargar/dotfiles/main/macos/setup.sh)"
+```
+
--- tea.sh
+++ tea.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 echo "go make some tea"
 # TODO: Parameterize for different envs
-conda run --name dotfiles --no-capture-output \
-	ansible-playbook playbooks/home.playbook.yml \
-	--ask-become-pass
+if [ "$(uname -s)" -eq "Linux" ]
+then
+	conda run --name dotfiles --no-capture-output \
+		ansible-playbook playbooks/home.playbook.yml \
+		--ask-become-pass
+else
+	conda run --name dotfiles --no-capture-output \
+		ansible-playbook playbooks/work.playbook.yml \
+fi
