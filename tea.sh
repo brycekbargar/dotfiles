130604b10288e92d7ec171a5225dd836e19c37d5
bryce <brycekbargar@gmail.com>
2022-08-21 08:47:48 -0400

    Fix macports install and use ansible.cfg

--- tea.sh
+++ tea.sh
@@ -3,10 +3,12 @@ echo "go make some tea"
 
 if [ "$(uname -s)" = "Linux" ]
 then
+	ANSIBLE_CONFIG="$(pwd)/playbooks/ansible.cfg" \
 	conda run --name dotfiles --no-capture-output \
 		ansible-playbook playbooks/home.playbook.yml \
 		--ask-become-pass
 else
+	ANSIBLE_CONFIG="$(pwd)/playbooks/ansible.cfg" \
 	conda run --name dotfiles --no-capture-output \
 		ansible-playbook playbooks/work.playbook.yml
 fi
