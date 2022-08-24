c71beb8111d40c8c0d405df3d7d452e431d5728a
bryce <brycekbargar@gmail.com>
2022-08-24 09:31:09 -0400

    Differentiate between home and work better (also become at work)

--- tea.sh
+++ tea.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 echo "go make some tea"
 
-if [ "$(uname -s)" = "Linux" ]
-then
-	ANSIBLE_CONFIG="$(pwd)/playbooks/ansible.cfg" \
-	conda run --name dotfiles --no-capture-output \
-		ansible-playbook playbooks/home.playbook.yml \
-		--ask-become-pass
-else
-	ANSIBLE_CONFIG="$(pwd)/playbooks/ansible.cfg" \
-	conda run --name dotfiles --no-capture-output \
-		ansible-playbook playbooks/work.playbook.yml
-fi
+playbook="playbooks/home.playbook.yml"
+[ "$(id -un)" = "katherine.bargar" ] && playbook="playbooks/work.playbook.yml"
+
+ANSIBLE_CONFIG="$(pwd)/playbooks/ansible.cfg" \
+conda run --name dotfiles --no-capture-output \
+	ansible-playbook "$playbook" \
+	--ask-become-pass
