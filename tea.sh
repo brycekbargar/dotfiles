6f8fd3c3825f78594b291b4be5aa6fc741a09aa6
bryce <brycekbargar@gmail.com>
2022-09-10 08:11:13 -0400

    Setup test user

--- tea.sh
+++ tea.sh
@@ -4,6 +4,9 @@ echo "go make some tea"
 playbook="playbooks/home.playbook.yml"
 [ "$(id -un)" = "katherine.bargar" ] && playbook="playbooks/work.playbook.yml"
 
+# testing on sickcodes-dockerosx
+[ "$(id -un)" = "user" ] && playbook="playbooks/work.playbook.yml"
+
 ANSIBLE_CONFIG="$(pwd)/playbooks/ansible.cfg" \
 conda run --name dotfiles --no-capture-output \
 	ansible-playbook "$playbook" \
