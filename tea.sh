1eecbeea4a92511b81d4956797f2640186f42c95
bryce <brycekbargar@gmail.com>
2023-02-11 16:27:40 -0500

    Remove tasks that are unnecessary for docker environment

--- tea.sh
+++ tea.sh
@@ -3,7 +3,7 @@ echo "go make some tea"
 
 # shellcheck disable=SC1090
 which conda >/dev/null || source <(~/.local/share/conda/base/bin/conda shell.zsh hook)
-conda env list | grep dotfiles -q || conda env create --file environment.yml --quiet
+conda env list | grep dotfiles -q || conda create --name dotfiles --file conda-linux-64.lock --quiet --yes
 
 ANSIBLE_CONFIG="$(pwd)/playbooks/ansible.cfg" \
 	conda run --name dotfiles --no-capture-output \
