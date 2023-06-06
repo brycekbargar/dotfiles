28a55eecd0d5a6cb127aa2b36ddb437d9d0444aa
bryce <brycekbargar@gmail.com>
2023-06-06 10:13:53 +0000

    Replace conda_run with default configurations for formatters

--- XDG_CONFIG_HOME/nvim/lua/config/neoformat.lua
+++ XDG_CONFIG_HOME/nvim/lua/config/neoformat.lua
@@ -2,118 +2,50 @@ vim.keymap.set("n", "<leader>f", ":Format<cr>", { noremap = true, silent = true 
 
 return function()
 	local format = require("formatter")
-	local util = require("formatter.util")
-	local conda_run = require("conda-run")
-
-	local function prettier(parser)
-		return {
-			function()
-				local p = conda_run.js({ n = "prettier" }).with_args({
-					"--stdin-filepath",
-					util.escape_path(util.get_current_buffer_file_path()),
-					"--parser",
-					parser,
-				})
-				p.stdin = true
-				return p
-			end,
-		}
+	local function filetype(formatter)
+		return require("formatter.filetypes." .. formatter)
 	end
 
 	format.setup({
 		filetype = {
-			lua = {
-				function()
-					local stylua = conda_run.exe({ n = "stylua" }).with_args({
-						"--stdin-filepath",
-						util.escape_path(util.get_current_buffer_file_path()),
-						"--",
-						"-",
-					})
-					stylua.stdin = true
-					return stylua
-				end,
-			},
+			json = { filetype("json").prettier },
+			jsonc = { filetype("json").prettier },
+			lua = { filetype("lua").stylua },
+			markdown = { filetype("markdown").prettier },
 
-			json = prettier("json"),
-			jsonc = prettier("json"),
-			yaml = prettier("yaml"),
-			["yaml.ansible"] = prettier("yaml"),
-			markdown = prettier("markdown"),
-
-			toml = {
+			ps1 = {
 				function()
-					local stylua = conda_run.exe({ n = "taplo" }).with_args({
-						"fmt",
-						"-",
-					})
-					stylua.stdin = true
-					return stylua
+					local InvokeFormatter = {
+						exe = "PowerShellEditorServices",
+						args = "/powershell_sa/Invoke-Formatter",
+					}
+					InvokeFormatter.stdin = true
+					return InvokeFormatter
 				end,
 			},
 
 			python = {
 				function()
-					local isort = conda_run.exe({ n = "isort" }).with_args({
-						"--profile",
-						"black",
-						"--quiet",
-						"-",
-					})
-					isort.stdin = true
+					local isort = filetype("python").isort
+					table.insert(isort.args, 1, "--profile")
+					table.insert(isort.args, 1, "black")
 					return isort
 				end,
-				function()
-					local black = conda_run.python({ n = "black" }).with_args({
-						"--stdin-filename",
-						util.escape_path(util.get_current_buffer_file_path()),
-						"--quiet",
-						"-",
-					})
-					black.stdin = true
-					return black
-				end,
+				filetype("python").black,
 			},
 
 			sh = {
 				function()
-					local shiftwidth = vim.opt.shiftwidth:get()
-					local expandtab = vim.opt.expandtab:get()
-
-					if not expandtab then
-						shiftwidth = 0
-					end
-
-					local shfmt = conda_run.exe({ n = "shfmt" }).with_args({
-						"-i",
-						shiftwidth,
-						"--simplify",
-					})
-					shfmt.stdin = true
+					local shfmt = filetype("bash").isort
+					table.insert(shfmt.args, 1, "--simplify")
 					return shfmt
 				end,
 			},
 
-			terraform = {
-				function()
-					local terraform = conda_run.exe({ n = "terraform" }).with_args({
-						"fmt",
-						"-",
-					})
-					terraform.stdin = true
-					return terraform
-				end,
-			},
-
-			ps1 = {
-				function()
-					local InvokeFormatter = conda_run.pwsh({
-						n = "/powershell_sa/Invoke-Formatter",
-					})
-					InvokeFormatter.stdin = true
-					return InvokeFormatter
-				end,
-			},
+			terraform = { filetype("terraform").terraformfmt },
+			toml = { filetype("toml").taplo },
+			yaml = { filetype("yaml").prettier },
+			["yaml.ansible"] = { filetype("yaml").prettier },
 		},
 	})
 end
