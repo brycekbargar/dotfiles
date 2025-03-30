f7d4679a6ee03e46f7b92f94c7855e16fd722f13
bryce <brycekbargar@gmail.com>
2025-03-30 22:57:48 +0000

    Modernize catpuccin config

--- XDG_CONFIG_HOME/nvim/lua/config/catppuccin.lua
+++ XDG_CONFIG_HOME/nvim/lua/config/catppuccin.lua
@@ -2,16 +2,43 @@ return function()
 	local catppuccin = require("catppuccin")
 
 	catppuccin.setup({
+		flavour = "frappe",
+		dim_inactive = { enabled = true },
+		default_integrations = false,
 		integrations = {
-			lsp_trouble = true,
 			indent_blankline = {
 				enabled = true,
 				colored_indent_levels = true,
 			},
+			native_lsp = {
+				enabled = true,
+				virtual_text = {
+					errors = { "italic" },
+					hints = { "italic" },
+					warnings = { "italic" },
+					information = { "italic" },
+					ok = { "italic" },
+				},
+				underlines = {
+					errors = { "underline" },
+					hints = { "underline" },
+					warnings = { "underline" },
+					information = { "underline" },
+					ok = { "underline" },
+				},
+				inlay_hints = {
+					background = true,
+				},
+			},
 			notify = true,
+			treesitter_context = true,
+			treesitter = true,
+			snacks = {
+				enabled = true,
+			},
+			lsp_trouble = true,
 		},
 	})
 
-	vim.g.catppuccin_flavour = "frappe"
 	vim.cmd([[colorscheme catppuccin]])
 end
